f4ca23b11b1e3e9b1ca0653f0c03f159209d3e60
mikygaleano <mikebostero.bernal@gmail.com>
2023-08-30 21:12:54 -0300

    count de tareas completadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { NewList } from "./components/NewList"
 import { CardsList } from "./components/CardsList";
 
@@ -6,8 +6,16 @@ import { CardsList } from "./components/CardsList";
 
 function App() {
   // almacenar las nuevas listas en localStorage
-  const [lists, setLists] = useState(JSON.parse(localStorage.getItem("lists")) || []);
+    const [lists, setLists] = useState(JSON.parse(localStorage.getItem("lists")) || []);
+    const [count, setCount] = useState(parseInt(localStorage.getItem("count")) || 0); // Obtener count de localStorage
   
+
+    useEffect(() => {
+      // Guardar count en localStorage cada vez que cambie
+      localStorage.setItem("count", count.toString());
+    }, [count]);
+
+
     const addList = (newList) => {
       const updatedLists = [...lists, newList];
       setLists(updatedLists);
@@ -15,19 +23,44 @@ function App() {
 
     const removeList = (index) => {
       const updatedLists = [...lists];
+
+      if (updatedLists[index].completed) {
+        setCount(count - 1); // Restar al contador solo si la tarea está marcada como completada
+      }
+      
       // splice elimina un elemento del array y devuelve un nuevo array
       updatedLists.splice(index, 1);
       setLists(updatedLists);
       localStorage.setItem("lists", JSON.stringify(updatedLists));
     };
+
+    const toggleCompleted = (index) => {
+      const updatedLists = [...lists];
+      updatedLists[index].completed = !updatedLists[index].completed;
+      setLists(updatedLists);
+      localStorage.setItem("lists", JSON.stringify(updatedLists));
+
+      // Actualizar el contador según el estado de la tarea completada
+      if (updatedLists[index].completed) {
+        setCount(count + 1);
+      } else if (count > 0) {
+        setCount(count - 1);
+      }
+    };
+
   
     return (
       <>
         <h1 className="text-center">To do list</h1>
         <NewList addList={addList} /> {/* Pasando addList aquí */}
         {lists.map((list, index) => (
-          <CardsList key={index} list={list} onDelete={() => removeList(index)} />
+          <CardsList 
+            key={index}
+            list={list}
+            onComplete={() => toggleCompleted(index)}
+            onDelete={() => removeList(index)} />
         ))}
+        <p>Completed: {count}</p>
       </>
     );
 }
--- src/components/CardsList.jsx
+++ src/components/CardsList.jsx
@@ -1,13 +1,21 @@
 
 
-export const CardsList = ({ list, onDelete })=> {
+export const CardsList = ({ list, onDelete, onComplete })=> {
 
     return (
-        <>
-            <h3>{list.title}</h3>
+        <div className={`border p-4 rounded-md shadow-md ${list.completed ? "bg-green-100" : "bg-white"}`}>
+            <h3 className={list.completed ? "line-through text-green-600" : ""}>{list.title}</h3>
             <p>Date: {list.date}</p>
             <p>Description: {list.description}</p>
+            <div className="flex items-center">
+                <input
+                type="checkbox"
+                checked={list.completed}
+                onChange={onComplete}
+                className="mr-2"
+                />
+            </div>
             <button onClick={onDelete}>Delete</button>
-        </>
+        </div>
     )
 }
